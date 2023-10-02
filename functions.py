8a82a91c680f94f57fd90e2d6eff25a7b923d82c
Pedro Rosa <devpedrorosa@gmail.com>
2023-10-02 20:37:19 -0300

    LOJA COM PROBLEMA NA ESCOLHAS DA MOEDAS

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
 # CLEAR THE TERMINAL - LIMPAR TERMINAL
 import os
+import time #time sleep
+
 def limpar_terminal():
+    time.sleep(4)
     if os.name == 'posix':
         os.system('clear')  # Comando clear para sistemas Unix (MacOS e Linux)
     elif os.name == 'nt':
@@ -14,8 +17,52 @@ class Jogador:
     def __init__(self, nome):
         self.nome = nome
         self.vida = 3  # Vida inicial 3
+        self.moeda = 1 # Moeda inicial 1
 
 def cadastrar_usuario(nome):
     jogador = Jogador(nome)
     return jogador
+
+
+#LOJA - SHOPPING
+
+def mostrar_loja():
+  from functions import cadastrar_usuario
+  while True:
+    print("=" * 100)
+    print("")
+    print("Bem-vindo à loja, aqui você pode usar suas moedas para comprar itens que podem ajudar durante o jogo.")
+    print("")
+    print("1. +1 Vida [2 Moedas]")
+    print("2. +1 Dica [3 Moedas]")
+    print("3. +1 Pulo [4 Moedas]")
+    print("4. [Sair da Loja]")
+    print("")
+    print("=" * 100)
+    print("")
+    
+    while True:
+        try:
+            escolha = int(input("Digite de 1 a 4 para escolher uma opção: "))
+            if escolha not in [1, 2, 3, 4]:
+                raise ValueError
+            break  # Sai do loop se o número válido for digitado
+        except ValueError:
+            print(f"Erro: apenas de 1 a 4.")
+            
+    if escolha == 1:
+      if jogador.moeda < 2:
+        print("Você não tem moedas suficientes!")
+      else:
+        jogador.vida += 1
+        jogador.moeda -= 2  # Deduz as moedas após a compra
+        print(f"Você comprou +1 Vida! Agora você tem {jogador.vida} vidas e {jogador.moeda} moedas.")
+
+
+  
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,39 +1,49 @@
 import time 
 from functions import limpar_terminal
 from functions import cadastrar_usuario
+from functions import mostrar_loja
 
 # INTRODUCTION - INTRODUÇÃO
-time.sleep(0.2)
-print('-' * 200)
+#time.sleep(0.2)
+print('=' * 100)
+print("")
 print("Bem vindos ao nome do game")
-time.sleep(2.5)
+print("")
+#time.sleep(2.5)
 print("O jogador terá inicialmente três tentativas para acertar corretamente as perguntas até chegar no one piece")
-print('-' * 200)
-time.sleep(6)
+print("")
+print('=' * 100)
 limpar_terminal()
 
 # CREATE A PLAYER - CRIANDO O JOGADOR
-# USUARIO = JOGADOR 
+# USUARIO = JOGADOR
+print("="*100) 
+print("")
 while True:
     try:
         nome_jogador = input("Digite o nome do jogador: ")
         if not nome_jogador:
-            raise ValueError("O nome do jogador não pode estar vazio.")
+            raise ValueError
         jogador = cadastrar_usuario(nome_jogador)
         break  # Sai do loop se um nome válido for digitado
-    except ValueError as error:
-        print(f"Erro: {error}. Por favor, digite um nome válido.")
+    except ValueError:
+        print("")
+        print(f"Erro: Por favor, digite um nome válido.")
+        print("")
+print("=" *100)
 
-time.sleep(2.2)
+#time.sleep(2.2)
 print(f"Olá, {jogador.nome}! Vamos começar nossa jornada? Espero que esteja pronto, porque o caminho não será nada fácil!")
-time.sleep(2.2)
+#time.sleep(2.2)
 print("Carregando...")
-time.sleep(3.5)
 limpar_terminal()
 
 # INITIAL GAME - INÍCIO DO GAME
 
 #PRIMEIRA PERGUNTA
-print('-' * 200)
+print('=' * 100)
 print(f"Jogador: {jogador.nome}")
 print(f"Vida: {jogador.vida}")
+print(f"Moeda: {jogador.moeda}")
+
+mostrar_loja()
